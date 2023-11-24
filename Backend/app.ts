0d70f3de7b3aa5dec7e7142c152a8281afe26a99
lasindukumara <lasindukumara2020@gmail.com>
2023-11-24 16:17:06 +0530

    create Server connection

--- Backend/app.ts
+++ Backend/app.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
--- Backend/server.ts
+++ Backend/server.ts
@@ -1,16 +1,4 @@
-import express from  "express";
-import { Request, Response } from 'express';
-import dotenv from 'dotenv';
 
-const app = express();
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
-
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening at http://localhost:${process.env.PORT}`);
-});
 
 
 
